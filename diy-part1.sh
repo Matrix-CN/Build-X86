e599850ba17d3ddb9fc83ac38d00359bdeaa2e68
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2021-08-29 18:21:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -63,3 +63,52 @@ rm -rf ../lean/luci-app-unblockneteasemusic
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
+
+# Svn checkout packages from immortalwrt's repository
+pushd customfeeds
+mkdir temp
+git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 temp/packages
+git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-18.06-k5.4 temp/luci
+
+# Add luci-app-cpufreq
+cp -r temp/luci/applications/luci-app-cpufreq luci/applications/luci-app-cpufreq
+rm -rf ../package/lean/luci-app-cpufreq
+
+# Add luci-app-cpulimit
+cp -r temp/luci/applications/luci-app-cpulimit luci/applications/luci-app-cpulimit
+cp -r temp/packages/utils/cpulimit packages/cpulimit
+
+# Add luci-app-eqos
+cp -r temp/luci/applications/luci-app-eqos luci/applications/luci-app-eqos
+
+# Add luci-app-gowebdav
+cp -r temp/luci/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+cp -r temp/packages/net/gowebdav packages/net/gowebdav
+
+# Add luci-proto-modemmanager
+cp -r temp/luci/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
+
+# Add netdata
+rm -rf packages/admin/netdata
+rm -rf ../package/lean/luci-app-netdata
+cp -r temp/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
+cp -r temp/packages/admin/netdata packages/admin/netdata
+
+# Add luci-app-smartdns
+cp -r temp/luci/applications/luci-app-smartdns luci/applications/luci-app-smartdns
+cp -r temp/packages/net/smartdns packages/net/smartdns
+
+# Set to local feeds
+pushd customfeeds/packages
+export packages_feed="$(pwd)"
+popd
+pushd customfeeds/luci
+export luci_feed="$(pwd)"
+popd
+sed -i '/src-git packages/d' feeds.conf.default
+echo "src-link packages $packages_feed" >> feeds.conf.default
+sed -i '/src-git luci/d' feeds.conf.default
+echo "src-link luci $luci_feed" >> feeds.conf.default
+
+# Update feeds
+./scripts/feeds update -a
