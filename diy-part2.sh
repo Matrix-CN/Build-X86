2f33b29482a072340fa251bc4b126bd91f34556a
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2022-11-18 13:01:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,18 +13,11 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Add build date to index page
 export orig_version="$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" package/lean/default-settings/files/zzz-default-settings
-
-# luci-app-cpufreq
-#sed -i "s/@arm/@(arm||aarch64)/g" ./feeds/luci/applications/luci-app-cpufreq/Makefile
-#sed -i "s/"services"/"system"/g" ./feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
-# Add cpufreq
-#rm -rf ./feeds/luci/applications/luci-app-cpufreq 
-#svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
 
 # Add luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -37,15 +30,13 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 # Add openclash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
 
-# Add bypass
-git clone --depth=1 https://github.com/kiddin9/openwrt-bypass.git package/luci-app-bypass
 
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-app-dockerman
-rm -rf feeds/luci/applications/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+#rm -rf feeds/luci/applications/luci-app-dockerman
+#git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
