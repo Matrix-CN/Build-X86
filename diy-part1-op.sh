466ac41bde555d65ea3652e3719c26030db78986
Ming <57690855+ZZM365-CN@users.noreply.github.com>
2023-09-18 13:04:53 +0800

    Update diy-part1-op.sh

--- diy-part1-op.sh
+++ diy-part1-op.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
@@ -17,7 +18,13 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> "feeds.conf.default"
-echo "src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> "feeds.conf.default"
-echo "src-git passwall2luci https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> "feeds.conf.default"
+#echo "src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> "feeds.conf.default"
+#echo "src-git passwall2luci https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
+cat >>feeds.conf.default <<EOF
+src-git helloworld https://github.com/fw876/helloworld
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages
+src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci
+src-git passwall2luci https://github.com/xiaorouji/openwrt-passwall2.git
+EOF
