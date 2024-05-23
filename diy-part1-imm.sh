cf73ffe44caa03aa1d8331b67c4204c106aee72d
Ming <57690855+ZZM365-CN@users.noreply.github.com>
2024-05-23 21:45:50 +0800

    Update diy-part1-imm.sh

--- diy-part1-imm.sh
+++ diy-part1-imm.sh
@@ -16,3 +16,22 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 科学上网插件
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+./scripts/feeds update -a
+\rm -rf feeds/luci/applications/luci-app-passwall
+./scripts/feeds install -a
+
+# echo '### Argon Theme Config ###'
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b master  https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config # if have
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+./scripts/feeds update -a
+./scripts/feeds install -a
