dfa766e8c4ad30b42d5ab3bcbce139df74a8dffb
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2024-09-02 11:23:40 +0800

    Update diy-part1-op.sh

--- scripts/diy-part1-op.sh
+++ scripts/diy-part1-op.sh
@@ -19,3 +19,4 @@
 echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/lisaac/luci-app-diskman.git;master' >> feeds.conf.default
