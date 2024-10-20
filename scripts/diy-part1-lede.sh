56b853f0e04f7f4c09f39f58a42529956e70b92c
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2024-10-20 21:15:23 +0800

    Update diy-part1-lede.sh

--- scripts/diy-part1-lede.sh
+++ scripts/diy-part1-lede.sh
@@ -21,7 +21,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
 
 #Add iStore
 #echo >> feeds.conf.default
