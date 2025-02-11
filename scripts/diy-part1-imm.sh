f3c1fe227ea52dfe47fff1d117671020de1414f1
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2025-02-11 10:26:00 +0800

    Update diy-part1-imm.sh

--- scripts/diy-part1-imm.sh
+++ scripts/diy-part1-imm.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
-#echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
