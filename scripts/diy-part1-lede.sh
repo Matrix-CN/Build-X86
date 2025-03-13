9c730f10f6c1bbc4a2d08f93d6abc9a473a5d500
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2025-03-13 21:10:03 +0800

    Update diy-part1-lede.sh

--- scripts/diy-part1-lede.sh
+++ scripts/diy-part1-lede.sh
@@ -18,6 +18,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add a feed source
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
