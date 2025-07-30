cf88de8d3f67de24d8f4a7bc56123341e3528d16
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2025-07-31 07:50:37 +0800

    Update diy-part1-imm-24.10.sh

--- scripts/diy-part1-imm-24.10.sh
+++ scripts/diy-part1-imm-24.10.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+#echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
