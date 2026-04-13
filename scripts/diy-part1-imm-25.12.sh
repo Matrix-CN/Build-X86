74ce9ec6ebd41cf470d41fb1e34d3de8bc063eaa
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2026-04-13 15:16:04 +0800

    Update diy-part1-imm-25.12.sh

--- scripts/diy-part1-imm-25.12.sh
+++ scripts/diy-part1-imm-25.12.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
