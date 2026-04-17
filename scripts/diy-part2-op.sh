4c05e70edf11697f2fe80a469cfa5e860934d1b2
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2026-04-17 14:44:10 +0800

    Update diy-part2-op.sh

--- scripts/diy-part2-op.sh
+++ scripts/diy-part2-op.sh
@@ -27,12 +27,6 @@ sed -i "/timezone='.*'/a\set system.@system[-1].zonename='Asia/Shanghai'" packag
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i 's/ImmortalWrt/X86/g' package/base-files/files/bin/config_generate
 
-#Add luci-app-eqosplus
-git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
-
-#Add luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-
 # autocore
 git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/autocore-arm package/system/autocore
 
