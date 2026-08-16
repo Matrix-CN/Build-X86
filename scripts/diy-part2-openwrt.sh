f77601f97943625ba6120d4ce1b3d9bf557ad3ce
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2026-08-16 10:20:04 +0800

    Update diy-part2-openwrt.sh

--- scripts/diy-part2-openwrt.sh
+++ scripts/diy-part2-openwrt.sh
@@ -27,6 +27,10 @@ sed -i "/timezone='.*'/a\set system.@system[-1].zonename='Asia/Shanghai'" packag
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i 's/ImmortalWrt/X86/g' package/base-files/files/bin/config_generate
 
+# Add luci-app-bandix
+git clone https://github.com/timsaya/openwrt-bandix.git package/bandix
+git clone https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
+
 # autocore
 git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/autocore-arm package/system/autocore
 
