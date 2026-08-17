5409d151ab628762597c060a63efd06f4edb3c54
Matrix <57690855+Matrix-CN@users.noreply.github.com>
2026-08-17 09:58:44 +0800

    Update diy-part1-immortalwrt.sh

--- scripts/diy-part1-immortalwrt.sh
+++ scripts/diy-part1-immortalwrt.sh
@@ -19,4 +19,3 @@
 #echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-echo 'src-git Bandix https://github.com/timsaya/luci-app-bandix' >>feeds.conf.default
